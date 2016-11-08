48e77706e7a803c4acff68db5377b3a199984641
Dominick Washburn <domwashburn@us.ibm.com>
2016-11-08 17:56:18 -0600

    initial commit, the plugin works though

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 var postcss = require('postcss');
 
-module.exports = postcss.plugin('PLUGIN_NAME', function (opts) {
+module.exports = postcss.plugin('postcss-parent-selector', function(opts) {
     opts = opts || {};
 
     // Work with options here
 
-    return function (root, result) {
-
-        // Transform CSS AST here
-
+    return function(root, result) {
+        root.walkRules(rule => {
+            rule.selectors = rule.selectors.map(selector => {
+                return selector.split(/,[\s]* /g).map( selector => {git co
+                    var newsSelector = `${opts.selector} ${selector}`
+                    return newsSelector
+                });
+            });
+        });
     };
 });
