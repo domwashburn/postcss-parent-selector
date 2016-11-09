4a1549bdb3dd2f35a29238ebb7d0be33cb078140
Dominick Washburn <domwashburn@us.ibm.com>
2016-11-09 09:56:38 -0600

    fixed error where the parent class was being added to @keyframe contents

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ module.exports = postcss.plugin('postcss-parent-selector', function(opts) {
 
     return function(root, result) {
         root.walkRules(rule => {
+            if (rule.parent && rule.parent.type === 'atrule' &&
+                rule.parent.name.indexOf('keyframes') !== -1) {
+                return
+            }
             rule.selectors = rule.selectors.map(selector => {
-                return selector.split(/,[\s]* /g).map( selector => {git co
+                return selector.split(/,[\s]* /g).map( selector => {
                     var newsSelector = `${opts.selector} ${selector}`
                     return newsSelector
                 });
